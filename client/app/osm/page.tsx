6660e192211411647263bc6cf327e053dea4b165
Jett Chen <jettchen12345@gmail.com>
2024-06-01 16:55:41 +0800

    transition interpolation

--- client/app/osm/page.tsx
+++ client/app/osm/page.tsx
@@ -3,7 +3,11 @@
 import { Chatbox } from "@/components/chatbox";
 import { ChatMessages } from "@/components/chat-messages";
 import ky from "ky";
-import DeckGL, { GeoJsonLayer } from "deck.gl";
+import DeckGL, {
+  FlyToInterpolator,
+  GeoJsonLayer,
+  WebMercatorViewport,
+} from "deck.gl";
 import { Map } from "react-map-gl";
 import { INITIAL_VIEW_STATE, MAPBOX_TOKEN } from "@/lib/constants";
 import osmtogeojson from "osmtogeojson";
@@ -12,37 +16,40 @@ import "mapbox-gl/dist/mapbox-gl.css";
 import { readStreamableValue, useActions, useUIState } from "ai/rsc";
 import { AI, ClientMessage } from "../actions";
 import { nanoid } from "ai";
+import { bbox } from "@turf/bbox";
 
 const OVERPASS_URL = "https://overpass-api.de/api/interpreter";
 
+const queryOsm = async (content: string) => {
+  const osm_codeblock = content
+    .split("```overpassql\n")
+    .at(-1)
+    ?.split("\n```")
+    .at(0);
+  if (!osm_codeblock) return;
+  console.log(osm_codeblock);
+  const result = await ky
+    .post(OVERPASS_URL, {
+      body: "data=" + encodeURIComponent(osm_codeblock),
+      timeout: false,
+    })
+    .json()
+    .catch((e) => {
+      console.log(e);
+    });
+  console.log(`result: ${result}`);
+  return osmtogeojson(result);
+};
+
 export default function OSM() {
-  const [geojsonData, setGeojsonData] = useState(null);
+  const [geojsonData, setGeojsonData] = useState<GeoJSON.FeatureCollection>({
+    type: "FeatureCollection",
+    features: [],
+  });
   const [input, setInput] = useState("");
   const [conversation, setConversation] = useUIState<typeof AI>();
   const { sendMessage } = useActions<typeof AI>();
-
-  const queryOsm = async (content: string) => {
-    const osm_codeblock = content
-      .split("```overpassql\n")
-      .at(-1)
-      ?.split("\n```")
-      .at(0);
-    if (!osm_codeblock) return;
-    console.log(osm_codeblock);
-    const result = await ky
-      .post(OVERPASS_URL, {
-        body: "data=" + encodeURIComponent(osm_codeblock),
-        timeout: false,
-      })
-      .json()
-      .catch((e) => {
-        console.log(e);
-      });
-    console.log(`result: ${result}`);
-    const geojson = osmtogeojson(result);
-    console.log(geojson);
-    setGeojsonData(geojson);
-  };
+  const [viewState, setViewState] = useState(INITIAL_VIEW_STATE);
 
   const handleSubmit = async () => {
     setConversation((prev: ClientMessage[]) => [
@@ -76,30 +83,46 @@ export default function OSM() {
     for await (const progress of readStreamableValue(progressStream)) {
       console.log(progress);
       if (progress?.kind === "done") {
-        queryOsm(progress.value);
+        const geojson = await queryOsm(progress.value);
+        if (geojson) {
+          setGeojsonData(geojson);
+          const [minLng, minLat, maxLng, maxLat] = bbox(geojson);
+          const vp = layer.context.viewport as WebMercatorViewport;
+          const { longitude, latitude, zoom } = vp.fitBounds(
+            [
+              [minLng, minLat],
+              [maxLng, maxLat],
+            ],
+            { padding: 100 }
+          );
+          setViewState({
+            longitude,
+            latitude,
+            zoom: Math.max(zoom - 1, 2),
+            transitionInterpolator: new FlyToInterpolator({ speed: 2 }),
+            transitionDuration: "auto",
+          });
+        }
       }
     }
   };
 
-  const layers = [
-    geojsonData &&
-      new GeoJsonLayer({
-        id: "geojson-layer",
-        data: geojsonData,
-        pickable: true,
-        stroked: true,
-        filled: true,
-        extruded: true,
-        pointType: "circle",
-        getFillColor: [255, 165, 0, 200], // Changed to a more orangy color
-        getLineColor: [255, 255, 255],
-        getPointRadius: 10,
-        pointRadiusMinPixels: 3,
-        getLineWidth: 2,
-        lineWidthMinPixels: 3,
-        getElevation: 30,
-      }),
-  ];
+  const layer = new GeoJsonLayer({
+    id: "geojson-layer",
+    data: geojsonData,
+    pickable: true,
+    stroked: true,
+    filled: true,
+    extruded: true,
+    pointType: "circle",
+    getFillColor: [255, 165, 0, 200], // Changed to a more orangy color
+    getLineColor: [255, 255, 255],
+    getPointRadius: 10,
+    pointRadiusMinPixels: 3,
+    getLineWidth: 2,
+    lineWidthMinPixels: 3,
+    getElevation: 30,
+  });
 
   return (
     <div className="w-full h-screen flex flex-row gap-3">
@@ -113,11 +136,7 @@ export default function OSM() {
       </div>
       <div className="flex-1 p-3">
         <div className="h-full relative">
-          <DeckGL
-            initialViewState={INITIAL_VIEW_STATE}
-            controller
-            layers={layers}
-          >
+          <DeckGL initialViewState={viewState} controller layers={[layer]}>
             <Map
               mapStyle="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
               mapboxAccessToken={MAPBOX_TOKEN}
