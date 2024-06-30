3ae0907039c95faf99a90fea31ee9083c4ee11e2
Jett Chen <jettchen12345@gmail.com>
2024-06-30 16:08:19 +0800

    Colorful Toasters

--- client/app/layout.tsx
+++ client/app/layout.tsx
@@ -52,7 +52,7 @@ export default function RootLayout({
             <DefaultKBar />
             <Analytics />
           </main>
-          <Toaster />
+          <Toaster richColors />
         </body>
       </html>
     </ClerkProvider>
